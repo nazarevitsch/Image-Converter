315efb9a343c848afef5c292b371fb080e837de4
Balatsenko Oleksandr <oleksandr.balatsenko@inbase.com.ua>
2021-04-04 22:55:40 +0300

    release

---
+++ Converter/ArgParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Converter
+{
+    public class ArgParser
+    {
+        public static Dictionary<string, string> Parse(string[] args)
+        {
+            var result = new Dictionary<string, string>(4) ;
+
+            if (args.Length < 4)
+            {
+                throw new Exception("You must pass at least 2 args --source and --goal-format");
+            }
+            
+            for (var arg = 0; arg < args.Length; arg+=2)
+            {
+                if (args[arg] == "--source")
+                {
+                    if (args[arg + 1] == "--goal-format" || args[arg + 1] == "--output")
+                    {
+                        throw new Exception("Wrong input");
+                    }
+
+                    result["source"] = args[arg + 1].ToLower();
+                }
+
+                if (args[arg] == "--output")
+                {
+                    if (args[arg + 1] == "--goal-format" || args[arg + 1] == "--source")
+                    {
+                        throw new Exception("Wrong input");
+                    }
+                    result["output"] = args[arg + 1];
+                }
+                
+                if (args[arg] == "--goal-format")
+                {
+                    if (args[arg + 1] == "--output" || args[arg + 1] == "--source")
+                    {
+                        throw new Exception("Wrong input");
+                    }
+                    result["goal-format"] = args[arg + 1].ToLower();
+                }
+            }
+            return result;
+        }
+    }
+}
--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,28 +1,69 @@
 ﻿
+using System;
+using System.IO;
+
 namespace Converter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            PngReader pngReader = new PngReader();
-            GifReader gifReader = new GifReader();
-            IImageReader ppmReader = new PpmReader();
-            Image.Image dog = pngReader.ReadImage(@"D:\Work\ImageConvertor\Converter\resources\Big.png");
-            Image.Image bananaGif = gifReader.ReadImage(@"D:\Work\ImageConvertor\Converter\resources\banana.gif");
-            Image.Image ppm = ppmReader.ReadImage(@"D:\Work\ImageConvertor\Converter\resources\dog.ppm");
+            var parsedArgs = ArgParser.Parse(args);
+
+            IImageReader reader;
+            IImageWriter writer;
             
-            IImageWriter writer = new PpmWriter();
-            IImageWriter bmpWriter = new BmpWriter();
-            writer.WriteImage(dog, @"D:\Work\ImageConvertor\Converter\resources\Big.ppm");
-            bmpWriter.WriteImage(dog, @"D:\Work\ImageConvertor\Converter\resources\Big.bmp");
+            if (!parsedArgs.ContainsKey("source") || !parsedArgs.ContainsKey("goal-format"))
+            {
+                var sourceErrString = parsedArgs.ContainsKey("source") ? "" : "--source";
+                var goalFormatErrString = parsedArgs.ContainsKey("goal-format") ? "" : "--goal-format";
+                
+                throw new Exception($"Args not contains {sourceErrString} {goalFormatErrString}");
+            }
             
-            writer.WriteImage(bananaGif, @"D:\Work\ImageConvertor\Converter\resources\banana.ppm");
-            bmpWriter.WriteImage(bananaGif, @"D:\Work\ImageConvertor\Converter\resources\banana.bmp");
-            
-            writer.WriteImage(ppm, @"D:\Work\ImageConvertor\Converter\resources\dogGIF.ppm");
-            bmpWriter.WriteImage(ppm, @"D:\Work\ImageConvertor\Converter\resources\dogGIF.bmp");
-            
+            var sourceName = Path.GetFileName(parsedArgs["source"]).Split('.');
+            if (sourceName.Length < 2) throw new Exception("Source must have extension");
+            if (!File.Exists(parsedArgs["source"]))
+            {
+                throw new Exception("File not exists!");
+            }
+            switch (sourceName[1])
+            {
+                case "png":
+                    reader = new PngReader();
+                    break;
+                case "ppm":
+                    reader = new PpmReader();
+                    break;
+                case "gif":
+                    reader = new GifReader();
+                    break;
+                case "bmp":
+                    reader = new BmpReader();
+                    break;
+                default:
+                    throw new Exception($"Format {sourceName[1]} not supported!");
+            }
+
+            var goalFormat = parsedArgs["goal-format"];
+            switch (goalFormat)
+            {
+                case "png":
+                    writer = new PngWriter();
+                    break;
+                case "ppm":
+                    writer = new PpmWriter();
+                    break;
+                case "bmp":
+                    writer = new BmpWriter();
+                    break;
+                default:
+                    throw new Exception($"Format {goalFormat} not supported!");
+            }
+
+            var image = reader.ReadImage(parsedArgs["source"]);
+            writer.WriteImage(image, parsedArgs.ContainsKey("output") ? parsedArgs["output"] : $"./convertedImage.{goalFormat}");
+            Console.WriteLine("Done");
         }
     }
 }
